938c9168dff9ee8e1e3d9ff20fa6a761d654ceb9
root <root@pve.forsteri.n5pro>
2026-04-01 18:22:39 +0000

    sync local changes before rebase

--- 07-PVE_PBS-CreateBackupJob.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-echo "[INFO] Wrapper de compatibilidade: 07 -> 06-PVE_PBS-CreateBackupJob.sh"
-exec bash "${SCRIPT_DIR}/06-PVE_PBS-CreateBackupJob.sh" "$@"
